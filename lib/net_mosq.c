f6d0ddcae019a299a8d520c7932bad3f811a9bb2
Roger A. Light <roger@atchoo.org>
2024-09-06 23:56:16 +0100

    Clear SSL error state to avoid spurious error reporting.
    Thanks to koranyellow. Closes #3054.

--- lib/net_mosq.c
+++ lib/net_mosq.c
@@ -976,6 +976,7 @@ ssize_t net__read(struct mosquitto *mosq, void *buf, size_t count)
 	errno = 0;
 #ifdef WITH_TLS
 	if(mosq->ssl){
+		ERR_clear_error();
 		ret = SSL_read(mosq->ssl, buf, (int)count);
 		if(ret <= 0){
 			ret = net__handle_ssl(mosq, ret);
@@ -1007,6 +1008,7 @@ ssize_t net__write(struct mosquitto *mosq, const void *buf, size_t count)
 	errno = 0;
 #ifdef WITH_TLS
 	if(mosq->ssl){
+		ERR_clear_error();
 		mosq->want_write = false;
 		ret = SSL_write(mosq->ssl, buf, (int)count);
 		if(ret < 0){
